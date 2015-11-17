d6f436c67b20182619941a02bdebede283c34229
nissaw <nissa.wollum@gmail.com>
2015-11-17 11:55:27 -0800

    db inside server folder

--- Server/db/UserModel.js
+++ Server/db/UserModel.js
@@ -1,1 +1,11 @@
+var mongoose = require('mongoose');
 
+var usesrSchema = new.mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  googlelogin: String
+})
+
+
+module.exports = mongoose.model('User', userSchema);
