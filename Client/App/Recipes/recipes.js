20fe82f352ea538c0f4ce90e3b7a64b0c1d3b068
mgaff <mgaffney123@gmail.com>
2015-11-17 17:49:21 -0500

    Fleshed out recipesHTML and service functions

--- Client/App/Recipes/recipes.js
+++ Client/App/Recipes/recipes.js
@@ -1,34 +1,46 @@
 angular.module('recipes', [])
 
-.controller('DisplayController', function ($scope, Search) {
+.controller('DisplayController', function ($scope, Search, $uibModal) {
   // Your code here
-  console.log("hello");
+  $scope.data = {}
   angular.extend($scope, Search);
   
-  $scope.displayData = function(data){
-    console.log("hello");
-      console.log(data);
-    //need to know format data is coming back in
-  }
-  Search.getRecipes('stuff', $scope.displayData);
-})
-.factory('Search', function ($http) {
-  // function will retreive recipes based off the search criteria 
-  var getRecipes = function(searchList, cb){
-    return $http({
-      method:'GET',
-      //TODO: get url from server
-      crossDomain: true,
-      //url: "http://api.yummly.com/v1/api/recipes?_app_id=886e5d3d&_app_key=60388a44a6049283ac4d260ecbb0cf24&q=soup"
-      url: "https://api.edamam.com/search?q=chicken&app_id=d5063256&app_key=8afe98dea10de5cb7dc5baebb0431f08"
-      //callback: cb
-    })
-    .then(function(resp){
-      cb(resp.data) 
+  $scope.getRecipes().then(function(recipes){
+    $scope.data.recipes = recipes;
+    console.log(recipes);
     });
+    //need to know format data is coming back in
+
+  Search.getRecipes('stuff', $scope.displayData);
+
+  $scope.open = function (recipe) {
+    $scope.recipe = recipe;
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'RecipeContent.html',
+      controller: 'RecipeInstanceCtrl',
+      task: task,
+      cat: cat,
+      resolve: {
+        items: function () {
+          return $scope.recipe
+        }
+      }
+  });
+}
+})
+.controller('RecipeInstanceCtrl', function ($scope, $uibModalInstance, recipe, ShoppingList) {
+
+  $scope.recipe = recipe
+
+  $scope.no = function(){
+    $uibModalInstance.close();
   }
-  return {
-    getRecipes: getRecipes
-  }
-  
+
+  $scope.ok = function () {
+    $uibModalInstance.close();
+    ShoppingList.addToList(recipe);
+    
+  };
+
 });
--- Client/App/Services/services.js
+++ Client/App/Services/services.js
@@ -1,20 +1,34 @@
-// var module =angular.module('recipe.services', [])
+var module =angular.module('recipe.services', [])
 
-// .factory('Search', function ($http) {
-//   // function will retreive recipes based off the search criteria 
-//   var getRecipes = function(searchList, cb){
-//     return $http({
-//       method:'GET',
-//       //TODO: get url from server
-//       url: "http://api.yummly.com/v1/api/recipes?_app_id=886e5d3d&_app_key=60388a44a6049283ac4d260ecbb0cf24&q=soup"
-//       data: searchList
-//     })
-//     .then(function(resp){
-//       cb(resp.data) 
-//     });
-//   }
-//   return {
-//     getRecipes: getRecipes
-//   }
+.factory('Search', function ($http) {
+  // function will retreive recipes based off the search criteria 
+  var getRecipes = function(searchList, cb){
+    return $http({
+      method:'GET',
+      //TODO: get url from server
+      url: "http://api.yummly.com/v1/api/recipes?_app_id=886e5d3d&_app_key=60388a44a6049283ac4d260ecbb0cf24&q=soup"
+      data: searchList
+    })
+    .then(function(resp){
+      cb(resp.data) 
+    });
+  }
+  return {
+    getRecipes: getRecipes
+  }
 
-// });
+});
+
+.factory('ShoppingList', function(){
+  var list = [];
+
+  var addToList = function(recipe){
+    list.push(recipe);
+  }
+
+  return {
+    list: list,
+    addToList: addToList
+  }
+  
+})
