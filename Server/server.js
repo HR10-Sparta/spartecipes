3d711c8abed4c242fa9102c644376ffb1ebd1ce7
Alex Zywiak <alex.zywiak@gmail.com>
2015-11-16 22:37:03 -0500

    (feat) Working on server structure.

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,14 @@
-server.js
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 8000;
+
+// mongoose connection?
+
+// Set up all the middleware
+require('./utils/middleware.js')(app, express);
+
+app.listen(port);
+
+console.log('Making digital magic on port ' + port);
