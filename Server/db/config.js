5276c8ae040b60c63a181fc2f81acfd0e0556ebe
nissaw <nissa.wollum@gmail.com>
2015-11-17 11:55:27 -0800

    db inside server folder

--- Server/db/config.js
+++ Server/db/config.js
@@ -1,1 +1,4 @@
-// For DB Configuration...
+var root = new Firebase("https://.....firbaseio.com");
+var recipeRef = new Firebase("https://.....firbaseio.com/recipes");
+var userRef = new Firebase("https://.....firbaseio.com/users");
+var listRef = new Firebase("https://.....firbaseio.com/lists");
---
+++ Server/db/recipeController.js
@@ -0,0 +1,1 @@
+recipeController.js
