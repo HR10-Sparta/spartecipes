2583d304e937b0d070f05431fb2be57796a2b26b
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-11-17 18:30:56 -0800

    Improved video

--- Client/search.js
+++ Client/search.js
@@ -1,3 +1,7 @@
+/*==========================================
+=            Search Bar Section            =
+==========================================*/
+
 // global keycode constant
 var KEYCODE_ESC = 27;
 // extending the jQuery prototype with setCursorPosition
@@ -44,3 +48,38 @@ $(document).ready(function() {
     }
   });
 });
+
+
+/*============================================
+=            Video Player Section            =
+============================================*/
+
+// Load the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Replace the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.
+  var player;
+  function onYouTubePlayerAPIReady() {
+    player = new YT.Player('video-big', {
+      videoId: 'Uj49PA8-L1E',
+      autoplay: 1,
+      controls: 0,
+      enablejsapi: 1,
+      end: 136,
+      start: 5,
+      fs: 0,
+      loop: 1,
+      modestbranding: 1,
+      playsinline: 1,
+      rel: 0,
+      showinfo: 0,
+      events: {
+        'onReady' : function (e) {e.target.playVideo(); },
+        'onStateChange' : function (e) {e.target.playVideo(); }
+      }
+    });
+  }
