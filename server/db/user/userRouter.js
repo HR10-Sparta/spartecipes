21929d7f0fa4f0274bbe1e198da18b0ca162a803
Alex Zywiak <alex.zywiak@gmail.com>
2015-11-18 12:39:05 -0500

    (fix) Added login to Google Route.  Added comments.

--- server/db/user/userRouter.js
+++ server/db/user/userRouter.js
@@ -3,22 +3,30 @@ var User = require('./userModel');
 /**
  * Routes for User Models on the DB
  * Handles all routes: '/api/users'
+ * 
+ * All Passport Strategies are in server/config/passport.js
  */
 module.exports = function(app, passport) {
 
+  // Handles Registration for a new user using passport local strategy
+  // Successful registration sends back DB user object into authenticate callback function
   app.route('/register')
     .post(function(req, res, next) {
       passport.authenticate('local-signup', function(err, user, info) {
+        // Signup error
         if (err) {
           return res.status(500).json({
             err: err
           });
         }
+        // No user returned
         if (!user) {
           return res.status(401).json({
             err: info
           });
         }
+        // Successfully registers new user
+        // Log them in
         req.logIn(user, function(err) {
           if (err) {
             return res.status(500).json({
@@ -32,7 +40,7 @@ module.exports = function(app, passport) {
       })(req, res, next);
     });
 
-  // Handles Local Login Strategy
+  // Handles Local Login Strategy for Returning Users
   app.route('/login')
     .post(function(req, res, next) {
       passport.authenticate('local-login', function(err, user, info) {
@@ -59,10 +67,15 @@ module.exports = function(app, passport) {
       })(req, res, next);
     });
 
+  // Initial Route for google Login
+  // Will redirect users to a google Auth page asking for access to the things in the scope array
+  // Saves new user to DB if successful
   app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
   }));
 
+  // Route called when google successfully authorizes user
+  // Successful registration sends back DB user object into authenticate callback function
   app.route('/auth/google/callback')
     .get(function(req, res, next) {
       passport.authenticate('google', function(err, user, info) {
@@ -77,20 +90,21 @@ module.exports = function(app, passport) {
           });
         }
         if (user) {
-          res.status(200).json({
-            status: 'you are in bro'
+          req.login(user, function(err) {
+            res.status(200).json({
+              status: 'you are in bro'
+            });
           });
         }
-        console.log(user);
       })(req, res, next);
     });
 
+  // Logout User
   app.route('/logout')
     .get(function(req, res, next) {
       req.logout();
       res.status(200).json({
         status: 'bye'
       });
-      console.log(req.user);
     });
 };
