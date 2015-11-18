e3cb23984d0b87fe36e1745ccc040152630c8d61
Alex Zywiak <alex.zywiak@gmail.com>
2015-11-17 19:38:24 -0500

    Changed server require path.

--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ var mongoose = require('mongoose');
 var app = express();
 
 // mongoose connection?
-mongoose.connect('mongodb://localhost/spartans');
+//mongoose.connect('mongodb://localhost/spartans');
+var port = process.env.PORT || 8000;
 
 /**
  * Set up Middleware
  */
 require('./config/middleware.js')(app, express);
 
+app.listen(port);
+console.log('Making digital magic on port ' + port);
 // Export app for index.js
 module.exports = app;
