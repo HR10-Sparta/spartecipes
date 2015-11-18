9fb3ef5b644bacdb8252b624ac668ca8b95df2ce
Alex Zywiak <alex.zywiak@gmail.com>
2015-11-17 22:08:23 -0500

    (feat) Completed Local Authentication Strategy and Routing

--- server/db/user/userRouter.js
+++ server/db/user/userRouter.js
@@ -1,4 +1,4 @@
-var userController = require('./userController');
+var User = require('./userModel');
 
 /**
  * Routes for User Models on the DB
@@ -6,30 +6,57 @@ var userController = require('./userController');
  */
 module.exports = function(app, passport) {
 
+  app.route('/register')
+    .post(function(req, res, next) {
+      passport.authenticate('local-signup', function(err, user, info) {
+        if (err) {
+          return res.status(500).json({
+            err: err
+          });
+        }
+        if (!user) {
+          return res.status(401).json({
+            err: info
+          });
+        }
+        req.logIn(user, function(err) {
+          if (err) {
+            return res.status(500).json({
+              err: 'Could not log in user'
+            });
+          }
+          res.status(200).json({
+            status: 'Login successful!'
+          });
+        });
+      })(req, res, next);
+    });
+
   // Handles Local Login Strategy
   app.route('/login')
-  	.post(function(req, res, next){
-  		passport.authenticate('local-login', function(err, user, info){
-  			if(err){
-  				return res.status(500).json({err:err});
-  			}
-  			if(!user){
-  				return res.status(401).json({err: info});
-  			}
-  			req.logIn(user, function(err){
-  				if(err){
-  					return res.status(500).json({err: 'Could not log in.'});
-  				}
-  				res.status(200).json({status: 'Login Successful!'});
-  			});
-  		})(req, res, next);
-  	});
+    .post(function(req, res, next) {
+      passport.authenticate('local-login', function(err, user, info) {
+        if (err) {
+          return res.status(500).json({
+            err: err
+          });
+        }
+        if (!user) {
+          return res.status(401).json({
+            err: info
+          });
+        }
+        req.logIn(user, function(err) {
+          if (err) {
+            return res.status(500).json({
+              err: 'Could not log in user'
+            });
+          }
+          res.status(200).json({
+            status: 'Login successful!'
+          });
+        });
+      })(req, res, next);
+    });
 
-  // Handles Local Signup Strategy
-  app.route('/signup')
-  	.post(function(req, res, next){
-  		passport.authenticate('local-signup', function(err, user, info){
-
-  		});
-  	});
 };
