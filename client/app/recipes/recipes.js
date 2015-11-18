159a87759171a8c36e9cde28cc66a4d8e7e51409
nissaw <nissa.wollum@gmail.com>
2015-11-18 12:33:11 -0800

    readded client/app/recipes/recipes.js

--- client/app/recipes/recipes.js
+++ client/app/recipes/recipes.js
@@ -1,53 +1,50 @@
 angular.module('recipes', ['recipe.services'])
 
+.controller('HeaderController', function($scope, Search) {
+    // Your code here
+    $scope.data = {};
+    //$scope.data.recipes = [];
+    angular.extend($scope, Search);
 
-.controller('HeaderController', function ($scope, Search) {
-  // Your code here
-  $scope.data = {};
-  //$scope.data.recipes = [];
-  angular.extend($scope, Search);
-  
-  $scope.retrieveRecipes = function (data) {
-    console.log("hello there");
-    Search.getRecipes(data).then(function (recipes) {
-      $scope.data.recipes = recipes;
-      console.log(recipes);
-    });
-  };
+    $scope.retrieveRecipes = function(data) {
+      console.log("hello there");
+      Search.getRecipes(data).then(function(recipes) {
+        $scope.data.recipes = recipes;
+        console.log(recipes);
+      });
+    };
 
-    
 
-  //Search.getRecipes('stuff', $scope.displayData);
 
-  $scope.open = function (recipe) {
-    $scope.recipe = recipe;
-    var modalInstance = $uibModal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'RecipeContent.html',
-      controller: 'RecipeInstanceCtrl',
-      task: task,
-      cat: cat,
-      resolve: {
-        items: function () {
-          return $scope.recipe;
+    //Search.getRecipes('stuff', $scope.displayData);
+
+    $scope.open = function(recipe) {
+      $scope.recipe = recipe;
+      var modalInstance = $uibModal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'RecipeContent.html',
+        controller: 'RecipeInstanceCtrl',
+        task: task,
+        cat: cat,
+        resolve: {
+          items: function() {
+            return $scope.recipe;
+          }
         }
-      }
-  });
-};
-})
-.controller('RecipeInstanceCtrl', function ($scope, $uibModalInstance, recipe, ShoppingList) {
+      });
+    };
+  })
+  .controller('RecipeInstanceCtrl', function($scope, $uibModalInstance, recipe, ShoppingList) {
 
-  $scope.recipe = recipe;
+    $scope.recipe = recipe;
 
-  $scope.no = function(){
-    $uibModalInstance.close();
-  };
+    $scope.no = function() {
+      $uibModalInstance.close();
+    };
 
-
-
-  $scope.ok = function() {
-    $uibModalInstance.close();
-    ShoppingList.addToList(recipe);
+    $scope.ok = function() {
+      $uibModalInstance.close();
+      ShoppingList.addToList(recipe);
 
     };
 
