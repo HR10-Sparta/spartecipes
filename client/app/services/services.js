425789f4fe5854ce1eb2b8e9fb35b5bb32406295
mgaff <mgaffney123@gmail.com>
2015-11-18 11:51:16 -0500

    (feat) fixed recipeID search url

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -15,14 +15,15 @@ var module =angular.module('recipe.services', [])
   var getSingleRecipe = function(recipeID){
     return $http({
       method:'GET',
-      url: "http://api.bigoven.com/recipes?any_kw=" + searchCriteria + "&pg=1&rpp=100&api_key=8hUGXs4S34zTaDWaG1CMMmjfB9I3M944"
+      url: "http://api.bigoven.com/recipe/{id}&api_key=8hUGXs4S34zTaDWaG1CMMmjfB9I3M944"
     })
     .then(function(resp){
       return resp.data.Results;
     });
   }
   return {
-    getRecipes: getRecipes
+    getRecipes: getRecipes,
+    getSingleRecipe: getSingleRecipe
   }
 
 })
