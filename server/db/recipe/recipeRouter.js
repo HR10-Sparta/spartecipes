ffc1065895fb5a2e924fff6e3300fd78b771bec5
Alex Zywiak <alex.zywiak@gmail.com>
2015-11-18 22:07:54 -0500

    (feat) Moved Big Oven API calls to server.  Changed factory to reference server.

--- server/db/recipe/recipeRouter.js
+++ server/db/recipe/recipeRouter.js
@@ -1,18 +1,40 @@
 var request = require('request');
 
-module.exports = function(app){
-	app.route('/search/:query')
-		.get(function(req, res, next){
-			res.send(req.params.query + '\n');
-		});
+module.exports = function(app) {
 
-	app.route('/:recipeId')
-		.get(function(req, res, next){
-			// req.params.recipeId
-		});
+	// Search for a query string
+  app.route('/search/:query')
+    .get(function(req, res, next) {
+      var query = "http://api.bigoven.com/recipes?any_kw=" + req.params.query + "&pg=1&rpp=100&api_key=8hUGXs4S34zTaDWaG1CMMmjfB9I3M944";
+      var options = {
+        url: query,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+      request(options, function(err, response, data) {
+        if (err) {
+          console.log(err);
+        }
+        res.send(data);
+      });
+    });
 
-	app.route('/params/:recipeId')
-		.get(function(req, res, next){
-			// req.params.recipeId
-		});
+  // Will find a single recipe by ID
+  app.route('/:recipeId')
+    .get(function(req, res, next) {
+      var query = "http://api.bigoven.com/recipe/" + req.params.recipeId + "?api_key=8hUGXs4S34zTaDWaG1CMMmjfB9I3M944";
+      var options = {
+        url: query,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+      request(options, function(err, response, data) {
+        if (err) {
+          console.log(err);
+        }
+        res.send(data);
+      });
+    });
 };
