c0b30c2c2a494b4b0e6072eef6f901d014fc3871
Alex Zywiak <alex.zywiak@gmail.com>
2015-11-19 11:52:23 -0500

    (feat) Added directive to catch and broadcast all key presses on document.

---
+++ client/app/directives/keypressEvents.js
@@ -0,0 +1,17 @@
+angular.module('recipes')
+  .directive('keypressEvents', [
+    '$document',
+    '$rootScope',
+    function($document, $rootScope) {
+      return {
+        restrict: 'A',
+        link: function() {
+          $document.bind('keypress', function(e) {
+            console.log('Got keypress:', e.which);
+            $rootScope.$broadcast('keypress', e);
+            $rootScope.$broadcast('keypress:' + e.which, e);
+          });
+        }
+      };
+    }
+  ]);
